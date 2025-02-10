70c2ca9e2e57278033097e50e8ea60ba8df0e14d
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2025-02-10 22:34:38 +0100

    modified:   ../../../build.gradle.kts 	new file:   application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.4.4"
     id("io.micronaut.aot") version "4.4.4"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("io.micronaut.data") version "4.4.4"
+    id("io.micronaut.test-resources") version "4.4.4"
+    id("io.micronaut.gradle-plugin") version "4.4.4"
+    id("java")
 }
 
 version = "0.1"
@@ -12,12 +17,42 @@ repositories {
 }
 
 dependencies {
-    annotationProcessor("io.micronaut:micronaut-http-validation")
-    annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
-    implementation("io.micronaut.serde:micronaut-serde-jackson")
-    compileOnly("io.micronaut:micronaut-http-client")
-    runtimeOnly("ch.qos.logback:logback-classic")
-    testImplementation("io.micronaut:micronaut-http-client")
+    // Micronaut JPA Hibernate
+    implementation("io.micronaut.data:micronaut-data-hibernate-jpa")
+
+    // Jakarta Persistence API
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+
+    // Micronaut JDBC & PostgreSQL Driver
+    implementation("io.micronaut.sql:micronaut-jdbc-hikari")
+    runtimeOnly("org.postgresql:postgresql:42.6.0")
+
+    // Micronaut Validation
+    implementation("io.micronaut:micronaut-validation")
+
+    // Micronaut Test Framework
+    testImplementation("io.micronaut.test:micronaut-test-junit5")
+
+    // Kotlin Support
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Micronaut Runtime & Configuration
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.data:micronaut-data-jpa")
+}
+
+micronaut {
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("org.zencode.*")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 
